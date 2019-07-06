71f416b38d192630d47cb57a49d3f483c6457e3f
JasmineChiehLi <jasmine_chieh_li@outlook.com>
2019-07-06 15:57:40 +0800

    add 1\2.wav, modify main program with a pseudo one

--- src/octaveAMT.py
+++ src/octaveAMT.py
@@ -12,16 +12,33 @@ def init_score(cur_score, path='./'):
     cur_score.audio_path = path
 
 
-if __name__ == '__main__':
-    # argv[1] should be the path of the audio source 
+def automatic_music_transcription(filepath='./'):
     score = Score()
+    init_score(score, filepath)
 
-    if len(sys.argv) > 1:
-        init_score(cur_score=score, path=sys.argv[1])
-    else:
-        init_score(cur_score=score)
     src_separate(score)
-
     onset_detect(score)
     pitch_detect(score)
     midi_generate(score)
+
+
+def pseudo_amt(filepath='./'):
+    score = Score()
+    init_score(score, filepath)
+
+    piano = InstrSeq()
+    piano.instr = "piano"
+    piano.audio_path = "1.wav"
+
+    score.instr_seqs = []
+    score.instr_seqs.append(piano)
+    onset_detect(score)
+
+
+if __name__ == '__main__':
+    # argv[1] should be the path of the audio source 
+
+    if len(sys.argv) > 1:
+        pseudo_amt(sys.argv[1])
+    else:
+        pseudo_amt()
