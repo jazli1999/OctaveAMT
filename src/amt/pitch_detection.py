a2f07b0dfd9b8f8c249e00ad7f97c0b11041800c
BY-Tang <45428652+BYTang314@users.noreply.github.com>
2019-07-13 17:19:32 +0800

    Update pitch_detection.py

--- src/amt/pitch_detection.py
+++ src/amt/pitch_detection.py
@@ -13,22 +13,25 @@ from keras.models import load_model
 
 def pitch_detect(sc):
     pm = "../cnn/CNN.model"  # temp
+    ps = "../cut/"
     classes = ['48', '53', '55', '57', '59', '60', '62', '64', '65', '72', '74', '76', '77', '79', '81']  # temp
     best_threshold = [0.1 for i in range(15)]  # temp
     model = load_model(pm)
 
-    for instr_seq in sc.instr_seqs:
+    for i1,instr_seq in enumerate(sc.instr_seqs):
         img = cv2.imread(instr_seq.spec_path, 1)
         # print(instr_seq.spec_path)
         # plt.imshow(img)
         valueto = math.ceil((instr_seq.notes[0].onset - 1) * sc.tempo)
         # print(valueto)
 
-        for note in instr_seq.notes:
+        for i2,note in enumerate(instr_seq.notes):
             onset = valueto
             valueto = math.ceil(onset + math.ceil(note.value * sc.tempo))
             # print(onset,valueto)
             imgtest = img[:, onset:valueto]
+            filename=("%d"+"."+"%d")%(i1,i2)+".jpg"
+            cv2.imwrite(ps+filename,imgtest)
             # plt.imshow(imgtest)
             imgtest = transform.resize(imgtest, (100, 100))
             imgtest = imgtest.transpose((2, 0, 1))
